11d781e1f7fba2434440574e96b942c29a83a466
Utkarsh Mandape <70505181+UtkarshM-hub@users.noreply.github.com>
2022-09-03 17:06:02 +0530

    Feat: Added Table View (#3)
    * Feat: Added Basic List Tasks command
    
    * Feat: Added Delete Task Feature
    
    * Feat: Added option for Description to Tasks
    
    * Fix: Some minor changes
    
    * Feat: Added Expand feature
    
    * Feat: Added Complete Task Command
    
    * Feat: Added Clear command
    
    * Feat: Added Table View

--- cmd/list.go
+++ cmd/list.go
@@ -4,11 +4,16 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	// "fmt"
+	"fmt"
+	"log"
+	"os"
+
+	// db "github.com/UtkarshM-hub/todo/DB"
 	db "github.com/UtkarshM-hub/todo/DB"
+	"github.com/olekukonko/tablewriter"
 	"github.com/spf13/cobra"
 )
-
-
 
 // listCmd represents the list command
 var listCmd = &cobra.Command{
@@ -16,7 +21,37 @@ var listCmd = &cobra.Command{
 	Short: "list all tasks",
 	Long: `List all tasks`,
 	Run: func(cmd *cobra.Command, args []string) {
-		db.ListAll()
+
+		tasks,err:=db.GetTasks()
+		if err!=nil{
+			log.Fatal("Error fetching data")
+		}
+
+		data :=[][]string{}
+
+		for index,item:=range tasks{
+			var prio string
+			if item.Priority<5{
+				prio="low"
+			} else if item.Priority>5{
+				prio="high"
+			} else {
+				prio="medium"
+			}
+			st:="In-Progress"
+			if(item.Status){
+				st="Completed"
+			}
+			data = append(data, []string{fmt.Sprint(index+1),item.Task,st,prio})
+		}
+
+		table := tablewriter.NewWriter(os.Stdout)
+		table.SetHeader([]string{"#", "Name", "Status","Priority"})
+
+		for _, v := range data {
+			table.Append(v)
+		}
+		table.Render()
 	},
 }
 
